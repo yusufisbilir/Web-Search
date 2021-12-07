4936fb470914fb47dd18d0936413b80a1ed755cf
yusufisbilir <dev.yusufisbilir@gmail.com>
2021-12-07 15:05:49 +0300

    Create Navbar component.

--- src/components/Navbar.jsx
+++ src/components/Navbar.jsx
@@ -1,3 +1,14 @@
+import { Link } from 'react-router-dom';
+import { Search } from './Search';
 export const Navbar = () => {
-  return <div>Navbar</div>;
+  return (
+    <div className="p-5 pb-0 flex flex-wrap sm:justify-between justify-center items-center border-b border-gray-200">
+      <Link to="/">
+        <p className="text-2xl bg-blue-500 font-bold text-white py-1 px-2 rounded-full">
+          Search
+        </p>
+      </Link>
+      <Search />
+    </div>
+  );
 };
