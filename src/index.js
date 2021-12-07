7fa2655ab1db05b251319d8a5d43675c474ca98c
yusufisbilir <dev.yusufisbilir@gmail.com>
2021-12-07 15:05:11 +0300

    Route App.js component in index.js.

--- src/index.js
+++ src/index.js
@@ -2,5 +2,13 @@ import React from 'react';
 import ReactDOM from 'react-dom';
 import './style.css';
 import App from './App';
+import { BrowserRouter, Routes, Route } from 'react-router-dom';
 
-ReactDOM.render(<App />, document.getElementById('root'));
+ReactDOM.render(
+  <BrowserRouter>
+    <Routes>
+      <Route path="/" element={<App />}></Route>
+    </Routes>
+  </BrowserRouter>,
+  document.getElementById('root')
+);
