f4aca3cc4102f85b818190938e54eb027ba12a02
yusufisbilir <dev.yusufisbilir@gmail.com>
2021-12-07 16:32:15 +0300

    Create Loading. Create Search section.

---
+++ src/components/Loading.jsx
@@ -0,0 +1,8 @@
+import Loader from 'react-loader-spinner';
+export const Loading = () => {
+  return (
+    <div className="flex justify-center items-center">
+      <Loader type="Puff" color="#00BFFF" height={550} width={80} />
+    </div>
+  );
+};
--- src/components/Results.jsx
+++ src/components/Results.jsx
@@ -1,3 +1,43 @@
+import React, { useEffect } from 'react';
+import { useLocation } from 'react-router-dom';
+import ReactPlayer from 'react-player';
+import { useResultContext } from '../contexts/ResultContextProvider';
+import { Loading } from './Loading';
+
 export const Results = () => {
-  return <div>results</div>;
+  const { results, isLoading, getResults, searchTerm } = useResultContext();
+  const location = useLocation();
+
+  useEffect(() => {
+    getResults('/search/q=Javascript&num=40');
+  }, []);
+
+  if (isLoading) return <Loading />;
+  switch (location.pathname) {
+    case '/search':
+      return (
+        <div className="flex flex-wrap justify-between space-y-6 sm:px-56 ">
+          {results?.results?.map(({ link, title }, index) => (
+            <div key={index} className="md:w-2/5 w-full">
+              <a href={link} target="_blank" rel="noreferrer">
+                <p className="text-sm">
+                  {link.length > 30 ? link.substring(0, 30) : link}
+                </p>
+                <p className="text-lg hover:underline dark:text-blue-300 text-blue-700">
+                  {title}
+                </p>
+              </a>
+            </div>
+          ))}
+        </div>
+      );
+    case '/images':
+      return 'images';
+    case '/news':
+      return 'news';
+    case '/videos':
+      return 'videos';
+    default:
+      return 'Error';
+  }
 };
