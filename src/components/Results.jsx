33000a11e3d0e13b2549fd486fb559bfa2ed6fad
yusufisbilir <dev.yusufisbilir@gmail.com>
2021-12-07 20:39:34 +0300

    Get results.

--- src/components/Results.jsx
+++ src/components/Results.jsx
@@ -9,15 +9,21 @@ export const Results = () => {
   const location = useLocation();
 
   useEffect(() => {
-    getResults('/search/q=Javascript&num=40');
-  }, []);
+    if (searchTerm) {
+      if (location.pathname === '/videos') {
+        getResults(`/search/q=${searchTerm} videos`);
+      } else {
+        getResults(`${location.pathname}/q=${searchTerm}&num=40`);
+      }
+    }
+  }, [searchTerm, location.pathname]);
 
   if (isLoading) return <Loading />;
   switch (location.pathname) {
     case '/search':
       return (
         <div className="flex flex-wrap justify-between space-y-6 sm:px-56 ">
-          {results?.results?.map(({ link, title }, index) => (
+          {results?.map(({ link, title }, index) => (
             <div key={index} className="md:w-2/5 w-full">
               <a href={link} target="_blank" rel="noreferrer">
                 <p className="text-sm">
@@ -32,11 +38,61 @@ export const Results = () => {
         </div>
       );
     case '/images':
-      return 'images';
+      return (
+        <div className="flex flex-wrap justify-center items-center">
+          {results?.map(({ image, link: { href, title } }, index) => (
+            <a
+              className="sm:p-3 p-5"
+              href={href}
+              key={index}
+              target="_blank"
+              rel="noreferrer"
+            >
+              <img src={image?.src} alt={title} loading="lazy" />
+              <p className="w-36 break-words text-sm mt-2">{title}</p>
+            </a>
+          ))}
+        </div>
+      );
     case '/news':
-      return 'news';
+      return (
+        <div className="flex flex-wrap justify-between space-y-6 sm:px-56 items-center">
+          {results?.map(({ links, id, source, title }) => (
+            <div key={id} className="md:w-2/5 w-full">
+              <a
+                href={links?.[0].href}
+                target="_blank"
+                rel="noreferrer"
+                className="hover:underline"
+              >
+                <p className="text-lg dark:text-blue-300 text-blue-700">
+                  {title}
+                </p>
+                <div className="flex gap-4">
+                  <a href={source?.href} target="_blank" rel="noreferrer">
+                    {source?.href}
+                  </a>
+                </div>
+              </a>
+            </div>
+          ))}
+        </div>
+      );
     case '/videos':
-      return 'videos';
+      return (
+        <div className="flex flex-wrap">
+          {results.map((video, index) => (
+            <div key={index} className="p-2">
+              <ReactPlayer
+                url={video.additional_links?.[0].href}
+                controls
+                width="355px"
+                height="200px"
+              />
+            </div>
+          ))}
+        </div>
+      );
     default:
       return 'Error';
   }
--- src/contexts/ResultContextProvider.js
+++ src/contexts/ResultContextProvider.js
@@ -6,7 +6,7 @@ const baseUrl = 'https://google-search3.p.rapidapi.com/api/v1';
 export const ResultContextProvider = ({ children }) => {
   const [results, setResults] = useState([]);
   const [isLoading, setIsLoading] = useState(false);
-  const [searchTerm, setSearchTerm] = useState('');
+  const [searchTerm, setSearchTerm] = useState('youtube');
 
   // type=> /videos /search /images
   const getResults = async (type) => {
@@ -22,7 +22,15 @@ export const ResultContextProvider = ({ children }) => {
       },
     });
     const data = await response.json();
-    setResults(data);
+
+    if (type.includes('/news')) {
+      setResults(data.entries);
+    } else if (type.includes('/images')) {
+      setResults(data.image_results);
+    } else {
+      setResults(data.results);
+    }
+
     setIsLoading(false);
   };
   return (
