087744dbdb28be70ca2cfaa74d11c96dbcc3cbf9
yusufisbilir <dev.yusufisbilir@gmail.com>
2021-12-07 15:45:39 +0300

    Create Routes.

--- src/components/Routes.jsx
+++ src/components/Routes.jsx
@@ -1,3 +1,17 @@
+import { Switch, Route, Redirect } from 'react-router-dom';
+import { Results } from './Results';
+
 export const Routes = () => {
-  return <div>Routes</div>;
+  return (
+    <div className="p-4">
+      <Switch>
+        <Route exact path="/">
+          <Redirect to="/search" />
+        </Route>
+        <Route path={['/search', '/news', '/images', '/videos']}>
+          <Results />
+        </Route>
+      </Switch>
+    </div>
+  );
 };
--- src/index.js
+++ src/index.js
@@ -2,13 +2,11 @@ import React from 'react';
 import ReactDOM from 'react-dom';
 import './style.css';
 import App from './App';
-import { BrowserRouter, Routes, Route } from 'react-router-dom';
+import { BrowserRouter } from 'react-router-dom';
 
 ReactDOM.render(
   <BrowserRouter>
-    <Routes>
-      <Route path="/" element={<App />}></Route>
-    </Routes>
+    <App />
   </BrowserRouter>,
   document.getElementById('root')
 );
